de584b06483298d41b87b6707ae8111f8025ad52
Ryan Price <ryanmichaelprice@gmail.com>
2019-07-26 23:58:18 -0400

    Payload types optional

--- src/Payload.ts
+++ src/Payload.ts
@@ -10,15 +10,15 @@ import User from 'ququmber-api/User';
 
 export default class Payload {
 
-  public lists: List[];
-  public listRoles: ListRole[];
-  public listRoleUsers: ListRoleUser[];
+  public lists?: List[];
+  public listRoles?: ListRole[];
+  public listRoleUsers?: ListRoleUser[];
 
-  public tasks: Task[];
-  public taskRoles: TaskRole[];
-  public taskRoleUsers: TaskRoleUser[];
-  public listTasks: ListTask[];
-  public recurrences: Recurrence[];
+  public tasks?: Task[];
+  public taskRoles?: TaskRole[];
+  public taskRoleUsers?: TaskRoleUser[];
+  public listTasks?: ListTask[];
+  public recurrences?: Recurrence[];
 
-  public users: User[];
+  public users?: User[];
 }
