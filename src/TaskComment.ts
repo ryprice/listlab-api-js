1f731e3fd41f698c5344fee71fcdfea8b2b3d0e5
Ryan Price <ryanmichaelprice@gmail.com>
2019-10-03 23:02:48 -0400

    TaskComment.clone

--- src/TaskComment.ts
+++ src/TaskComment.ts
@@ -4,4 +4,14 @@ export default class TaskComment {
   public userId: number;
   public text: string;
   public time: Date;
+
+  clone() {
+    const newTaskComment = new TaskComment();
+    newTaskComment.commentId = this.commentId;
+    newTaskComment.taskId = this.taskId;
+    newTaskComment.userId = this.userId;
+    newTaskComment.text = this.text;
+    newTaskComment.time = this.time;
+    return newTaskComment;
+  }
 }
