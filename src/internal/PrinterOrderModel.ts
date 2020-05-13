ef13b00751a722d6fe946e24f5690b700c93dcd2
Ryan Price <ryanmichaelprice@gmail.com>
2020-05-12 23:46:16 -0400

    Move shared printer app business logic to listlab-api

---
+++ src/internal/PrinterOrderModel.ts
@@ -0,0 +1,81 @@
+import Actor from 'listlab-api/Actor';
+
+export const PRODUCT_NAME = 'Printed daily A7 cards (4.25 x 2.75)';
+
+export const pricePerPage = (quantity: number) => {
+  const count = pageCount(quantity);
+  if (count <= 2) {
+    return 4.75;
+  } else if (count <= 4) {
+    return 3.75;
+  } else if (count <= 8) {
+    return 3.25;
+  } else {
+    return 2.70;
+  }
+};
+
+export const pageCount = (quantity: number) => Math.ceil(quantity/8);
+
+export const weightCost = (weight: PaperWeights) => {
+  if (weight === PaperWeights.EXTRAHEAVY) {
+    return 2.50;
+  } else {
+    return 0;
+  }
+};
+
+export const weightName = (weight: PaperWeights) => {
+  if (weight === PaperWeights.LIGHTPRINTER) {
+    return 'Lightweight';
+  } else if (weight === PaperWeights.CARDSTOCK) {
+    return 'Card stock';
+  } else if (weight === PaperWeights.EXTRAHEAVY) {
+    return 'Extra heavy';
+  }
+};
+
+export const shippingCost = () => {
+  return 3.00;
+};
+
+export const roundPrice = (price: number) => Math.round((price + Number.EPSILON) * 100) / 100;
+
+export const productPrice = (order: PrinterOrder) => roundPrice(
+  pricePerPage(order.quantity) * pageCount(order.quantity) +
+  weightCost(order.weight)
+);
+
+export const totalCost = (order: PrinterOrder) => roundPrice(
+  productPrice(order) +
+  shippingCost()
+);
+
+type PrinterOrderAddress = {
+  name: string;
+  line1: string;
+  line2: string;
+  city: string;
+  state: string;
+  postalCode: string;
+};
+
+export type PrinterOrder = {
+  orderId?: number,
+  quantity?: number,
+  weight?: number,
+  list?: string[],
+  email?: string,
+  address?: PrinterOrderAddress;
+  paypalOrderId?: string;
+  paypalTransactionId?: string;
+  actor?: Actor;
+  timeCompleted?: Date;
+  timeShipped?: Date;
+};
+
+export enum PaperWeights {
+  LIGHTPRINTER = 1,
+  CARDSTOCK = 2,
+  EXTRAHEAVY = 3,
+}
