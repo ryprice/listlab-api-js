e063a5b870129aa2a38c82d112f7340f4ed9804e
Ryan Price <ryanmichaelprice@gmail.com>
2020-05-26 21:09:46 -0400

    Rename .eslintrc to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+const {importOrderConfig} = require('listlab-build/eslintrcConfigBuilders');
+
+module.exports = {
+  "extends": "../listlab-build/.eslintrc",
+  "parserOptions": {
+    "project": ["./tsconfig.json"]
+  },
+  "rules": { 
+    "import/order": importOrderConfig('ququmber-ui'),
+  }
+};
