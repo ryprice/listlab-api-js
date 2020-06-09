ac432aea3cd605fbf4cc06a724fcb35d41e36bcc
Ryan Price <ryanmichaelprice@gmail.com>
2020-06-08 23:55:29 -0400

    Use enum for NotificationTypes

--- src/NotificationTypes.ts
+++ src/NotificationTypes.ts
@@ -1,11 +1,8 @@
-export const ASSIGNED_TASK = 'ASSIGNED_TASK';
-export const MESSAGE = 'MESSAGE';
-export const SHARED_LIST = 'SHARED_LIST';
-export const SHARED_TASK = 'SHARED_TASK';
-
-export default {
-  ASSIGNED_TASK,
-  MESSAGE,
-  SHARED_LIST,
-  SHARED_TASK
+enum NotificationTypes {
+  ASSIGNED_TASK = 'ASSIGNED_TASK',
+  MESSAGE = 'MESSAGE',
+  SHARED_LIST = 'SHARED_LIST',
+  SHARED_TASK = 'SHARED_TASK',
 };
+
+export default NotificationTypes;
