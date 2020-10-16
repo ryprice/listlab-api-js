96ce68c7a9100660ee9b07415f60bca280ae0be7
Ryan Price <ryanmichaelprice@gmail.com>
2020-10-15 23:55:53 -0400

    TaskFilter leaf and root options to find orphaned tasks and tasks at the root of the task tree or a subtree

--- src/TaskFilter.ts
+++ src/TaskFilter.ts
@@ -3,14 +3,47 @@ import {pickBy} from 'lodash';
 import FuzzyTimeRange from 'listlab-api/FuzzyTimeRange';
 
 export default class TaskFilter {
+
+  /**
+   * Includes only tasks recursively nested under this list
+   */
   public readonly listId: number;
+
   public readonly range: FuzzyTimeRange;
+
+  /**
+   * Includes only tasks recursively nested under this parent task (inclusive)
+   */
   public readonly parentId: number;
+
   public readonly completed: boolean;
+
   public readonly query: string;
+
   public readonly inProgress: boolean;
+
   public readonly seen: boolean;
+
+  /**
+   * Includes only tasks with taskIds in this array
+   */
   public readonly taskIds: number[];
+
+  /**
+   * When true, includes only tasks that have no children.
+   */
+  public readonly isLeaf: boolean;
+
+  /**
+   * When true, includes tasks at the root of the tree. If parentId
+   * is also set, includes only tasks that are at the root of the subtree.
+   * When false, include only non-root tasks.
+   * 
+   * Examples:
+   * {isRoot: true, isLeaf: true} results in orphaned tasks
+   * {isRoot: true, isLeaf: false} results in root level tasks with children
+   */
+  public readonly isRoot: boolean;
 
   constructor(init?: {[P in keyof TaskFilter]?: TaskFilter[P]}) {
     if (init) {
@@ -22,6 +55,8 @@ export default class TaskFilter {
       this.inProgress = init.inProgress;
       this.seen = init.seen;
       this.taskIds = init.taskIds;
+      this.isLeaf = init.isLeaf;
+      this.isRoot = init.isRoot;
     }
   }
 
@@ -53,6 +88,20 @@ export default class TaskFilter {
     });
   }
 
+  public setIsRoot(isRoot: boolean): TaskFilter {
+    return new TaskFilter({
+      ...(this as TaskFilter),
+      isRoot
+    });
+  }
+
+  public setIsLeaf(isLeaf: boolean): TaskFilter {
+    return new TaskFilter({
+      ...(this as TaskFilter),
+      isLeaf
+    });
+  }
+
   public isEmpty(): boolean {
     return (
       !this.listId &&
@@ -66,7 +115,9 @@ export default class TaskFilter {
         this.range.getStart() == null ||
         this.range.getEnd() == null
       ) &&
-      !this.taskIds
+      !this.taskIds &&
+      this.isLeaf == null &&
+      this.isRoot == null
     );
   }
 
@@ -79,7 +130,9 @@ export default class TaskFilter {
       this.query === other.query &&
       this.inProgress === other.inProgress &&
       this.seen === other.seen &&
-      this.taskIds === other.taskIds
+      this.taskIds === other.taskIds &&
+      this.isLeaf === other.isLeaf &&
+      this.isRoot === other.isRoot
     );
   }
 
