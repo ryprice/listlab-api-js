9c8fbfd7e8fa906c03005e1d6fb45560e54703b4
Ryan Price <ryanmichaelprice@gmail.com>
2021-01-29 23:51:51 -0500

    taskSortOrderLabel

--- src/TaskSortOrder.ts
+++ src/TaskSortOrder.ts
@@ -5,4 +5,13 @@ enum TaskSortOrder {
   CREATION_TIME = 'creation_time',
 }
 
+export const taskSortOrderLabel = (sortOrder: TaskSortOrder) => {
+  switch(sortOrder) {
+    case TaskSortOrder.PARENT_ORDER: return 'Parent/child';
+    case TaskSortOrder.DUE: return 'Due date';
+    case TaskSortOrder.CREATION_TIME: return 'Creation date';
+    case TaskSortOrder.COMPLETION_TIME: return 'Completion date';
+  }
+};
+
 export default TaskSortOrder;
